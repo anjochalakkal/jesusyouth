a40f7e51b768cf7204d3ac6f0e9f31b202d88bb9
Anjo <anjo_antony@geojit.com>
2026-02-15 14:52:55 +0530

    integrate firebase

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:jesusyouth/routes/app_router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(routerConfig: AppRouter.goRouter);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:jesusyouth/app.dart';
 import 'package:jesusyouth/di/injection_container.dart';
-import 'package:jesusyouth/routes/app_router.dart';
+import 'package:jesusyouth/firebase_options.dart';
 
 void main() async {
   await init();
+    WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: AppRouter.goRouter);
-  }
-}
